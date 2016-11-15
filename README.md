8c24a09038b7d5a4cb3bbaa67fd7b9ee218e8a34
James Lees <james@lees.io>
2016-11-15 14:21:40 +0000

    strip specifics from the script
    and add a note about product object

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ Utilising (needlessly) [vue.js](https://vuejs.org/).
 Run `yarn install` and `npm install` depending on package manager preference. 
 Run `npm run serve` to kick things off. Default port is 3456, changeable with
 env var `PORT`.
+
 ### Environment Variables
-The firebase config json `public/firebase-config.js` should be populated.
+The firebase config json `public/firebase-config.js` should be populated. A db
+ref should be entered in script.js too. There's a hardcoded 'dummy product' in
+reality this would be replaced with a reference to a firebase path.
 
--- public/script.js
+++ public/script.js
@@ -3,54 +3,51 @@
   var ref = db.ref('app/echo');
   var orders = ref.child('orders');
   var customers = ref.child('customers');
-  var products = ref.child('products');
   var payments = ref.child('payments');
 
-  // hardcoded product id
-  products.child('-KWc7KrVdvPe1M-xfK12').on('value', function(snapshot){
-    var product = snapshot.val();
-    var productId = snapshot.key;
-    new Vue({
-      el: '#app',
-      data: {
-        customer: {
-          email: 'james@lees.io',
-          name: 'james'
+  // hardcoded product, this would be obtained from a product list in firebase
+  var product = {price: 3.99};
+  var productId = 'someProductId';
+  new Vue({
+    el: '#app',
+    data: {
+      customer: {
+        email: 'james@lees.io',
+        name: 'james'
+      },
+      payment: {
+        details: {
+          cvc: '123',
+          expiry: '12/23',
+          number: '4242424242424242'
         },
-        payment: {
-          details: {
-            cvc: '123',
-            expiry: '12/23',
-            number: '4242424242424242'
-          },
-          amount: product.price,
-          currency: 'gbp',
-          type: 'card'
-        }
-      },
-      methods: {
-        submit: function(){
-          var orderId = orders.push().key;
-          var customerId = customers.push().key;
-          var paymentId = payments.push().key;
-          var customer = this.customer;
-          customer.payments = {};
-          customer.orders = {};
-          customer.orders[orderId] = true;
-          customer.payments[paymentId] = true;
-          customers.child(customerId).set(customer);
+        amount: product.price,
+        currency: 'gbp',
+        type: 'card'
+      }
+    },
+    methods: {
+      submit: function(){
+        var orderId = orders.push().key;
+        var customerId = customers.push().key;
+        var paymentId = payments.push().key;
+        var customer = this.customer;
+        customer.payments = {};
+        customer.orders = {};
+        customer.orders[orderId] = true;
+        customer.payments[paymentId] = true;
+        customers.child(customerId).set(customer);
 
-          var payment = this.payment;
-          payment.customer = customerId;
-          payments.child(paymentId).set(payment);
-          var order = {
-            customer: customerId,
-            payment: paymentId,
-            product: productId
-          };
-          orders.child(orderId).set(order);
-        }
-      },
-    });
+        var payment = this.payment;
+        payment.customer = customerId;
+        payments.child(paymentId).set(payment);
+        var order = {
+          customer: customerId,
+          payment: paymentId,
+          product: productId
+        };
+        orders.child(orderId).set(order);
+      }
+    },
   });
 })(Vue, firebase);
